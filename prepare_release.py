c8f4d2ba3e06c8262269d14a064e54eb74f8e0a6
Eric Prestat <eric.prestat@gmail.com>
2023-11-27 10:11:24 +0000

    Add `prepare_release` script to facilitate release: run towncrier and update fallback version

--- prepare_release.py
+++ prepare_release.py
@@ -14,19 +14,28 @@ def run_towncrier(tag):
 
 
 def update_fallback_version_in_pyproject(tag, fname="pyproject.toml"):
+    version = tag.strip("v").split(".")
+    # Default to +1 on minor version
+    major, minor = version[0], int(version[1]) + 1
+
     with open(fname, "r") as file:
         lines = file.readlines()
 
     pattern = "fallback_version"
+    new_version = f"{major}.{minor}.dev0"
     # Iterate through the lines and find the pattern
     for i, line in enumerate(lines):
         if re.search(pattern, line):
-            lines[i] = f"{pattern} = {tag}.dev0\n"
+            lines[i] = f'{pattern} = "{new_version}"\n'
             break
 
     # Write the updated content back to the file
     with open(fname, "w") as file:
         file.writelines(lines)
+
+    print(
+        f"\nNew (fallback) dev version ({new_version}) written to `pyproject.toml`.\n"
+    )
 
 
 if __name__ == "__main__":
